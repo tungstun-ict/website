b0a590223b5a673b59ac01e1b01d9086b7dc7a49
Jort Willemsen <jort@maurice-nienke.nl>
2023-01-18 22:31:25 +0100

    Add header component

---
+++ tungstun-website/src/components/header/header.tsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import { Menu } from "react-feather";
+
+import styles from "./header.module.scss";
+
+const Header = ({}) => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  return (
+    <div className={`${styles.container}`}>
+      <h2 className={`${styles.title}`}>tungstun</h2>
+      <div className={`${styles.menu} ${menuOpen && (styles.menu__open)}`}>
+        <nav className={`${styles.menu__links}`}>
+          <a>projects</a>
+          <a>photography</a>
+          <a>contact</a>
+        </nav>
+        <Menu 
+          onClick={() => {
+            setMenuOpen(!menuOpen); 
+            console.log("hi")
+          }}
+          className={`${styles.menu__icon}`} 
+          size={50} />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
