abfe23b8233197b62157a8603e0342a8f1fe5d28
Jort Willemsen <jort@maurice-nienke.nl>
2024-02-25 00:29:53 +0100

    Add vercel serverless

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
 import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
