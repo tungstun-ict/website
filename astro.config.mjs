bc9510a621e9d8c8ca831b3522117f807a5dade2
Jort Willemsen <jort@maurice-nienke.nl>
2024-03-14 10:34:00 +0100

    Add sitemaps

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,16 @@ import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
 import vercel from '@astrojs/vercel/serverless';
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), mdx()],
+  integrations: [react(), mdx(), sitemap()],
   site: "https://www.tungstun.nl",
   output: 'hybrid',
   adapter: vercel({
-    webAnalytics: { enabled: true }
-  }),
+    webAnalytics: {
+      enabled: true
+    }
+  })
 });
